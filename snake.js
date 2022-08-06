2e2b303feacade278e475a1e2c460615594f4ac5
August Hammers <augusthammers@gmail.com>
2022-08-06 16:24:46 -0400

    initial commit

---
+++ snake.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const canvasSize = 500;
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 150, 75);
+
+for(let i = 0; i < 500/15; i++) {
+  for (let j = 0; j < 500/15; i++) {
+    //ctx.fillRect(0, )
+  }
+}
