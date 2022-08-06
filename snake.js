35193f7b4e73350f1b0ab2ca432c40b0854dd183
August Hammers <augusthammers@gmail.com>
2022-08-06 18:19:46 -0400

    add movement and apple collision

--- snake.js
+++ snake.js
@@ -3,8 +3,14 @@ const ctx = canvas.getContext("2d");
 const boxSize = 25; //500px / 20 boxes
 const darkerBlue = "#00989d";
 const lighterBlue = "#75b3aa";
-let headXPos = 250; //increments of 25
+let gameSpeed = 15; //60 updates/second
+let headXPos = 250; 
 let headYPos = 250;
+let xVelocity = 0;
+let yVelocity = 0;
+let appleXPos = Math.floor(Math.random() * 19) * boxSize;
+let appleYPos = Math.floor(Math.random() * 19) * boxSize;
+let score = 0;
 
 function drawBoard() {
   ctx.fillStyle = darkerBlue;
@@ -23,22 +29,81 @@ function drawBoard() {
         ctx.fillStyle = lighterBlue;
       } else {
         ctx.fillStyle = darkerBlue;
-      }
-  
-      console.log(xPos, yPos);
+      }  
       ctx.fillRect(xPos, yPos, boxSize, boxSize);
     }
   }
 }
 
-function gameLoop() {
+function updateSnakePos(){
+  headXPos += xVelocity;
+  headYPos += yVelocity;
+}
+
+function updateApplePos(){
+  appleXPos = Math.floor(Math.random() * 19) * boxSize;
+  appleYPos = Math.floor(Math.random() * 19) * boxSize; 
+}
+
+function drawSnake(){
   ctx.fillStyle = "yellow";
   ctx.fillRect(headXPos+4, headYPos+4, boxSize-8, boxSize-8);
 }
 
-function setUp(){
-  drawBoard();
-  gameLoop();
+function drawApple(){
+  ctx.fillStyle = "red";
+  ctx.fillRect(appleXPos+4, appleYPos+4, boxSize-8, boxSize-8);
 }
 
-setUp();
+function gameLoop() {
+  drawBoard();
+  drawSnake();
+  drawApple();
+  document.addEventListener("keydown", (event) => {
+    event.preventDefault(); // To prevent arrow key scrolling
+    switch(event.key) {
+      case "ArrowUp":
+        if (yVelocity == 25) {
+          break;
+        }
+
+        xVelocity = 0;
+        yVelocity = -25;
+        break;
+      case "ArrowLeft":
+        if (xVelocity == 25) {
+          break;
+        }
+
+        xVelocity = -25;
+        yVelocity = 0;
+        break;
+      case "ArrowDown":
+        if (yVelocity == -25) {
+          break;
+        }
+
+        xVelocity = 0;
+        yVelocity = 25;
+        break;
+      case "ArrowRight":
+        if (xVelocity == -25) {
+          break;
+        }
+
+        xVelocity = 25;
+        yVelocity = 0;
+        break;
+      default:
+    }
+  });
+
+  updateSnakePos();
+  if (headXPos === appleXPos && headYPos === appleYPos) {
+    score++;
+    updateApplePos();
+  }
+  setTimeout(gameLoop, 1000/gameSpeed);
+}
+
+gameLoop();
