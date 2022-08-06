fa0cc0675c06ce905db33829be7456cada841c5a
August Hammers <augusthammers@gmail.com>
2022-08-06 17:25:15 -0400

    add drawBoard functionality

--- snake.js
+++ snake.js
@@ -1,12 +1,29 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const canvasSize = 500;
+const canvasHeight = canvas.height;
+const boxSize = 25; //500px / 20 boxes
+const darkerBlue = "#00989d";
+const lighterBlue = "#75b3aa";
 
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 150, 75);
-
-for(let i = 0; i < 500/15; i++) {
-  for (let j = 0; j < 500/15; i++) {
-    //ctx.fillRect(0, )
+function drawBoard() {
+  ctx.fillStyle = darkerBlue;
+  for(let i = 0; i < 20; i++) {
+    if (ctx.fillStyle === darkerBlue) {
+      ctx.fillStyle = lighterBlue;
+    } else {
+      ctx.fillStyle = darkerBlue;
+    }
+    let xPos = i * boxSize;
+    for (let j = 0; j < 20; j++) {
+      let yPos = j * boxSize;
+      if (ctx.fillStyle === darkerBlue) {
+        ctx.fillStyle = lighterBlue;
+      } else {
+        ctx.fillStyle = darkerBlue;
+      }
+  
+      console.log(xPos, yPos);
+      ctx.fillRect(xPos, yPos, boxSize, boxSize);
+    }
   }
 }
